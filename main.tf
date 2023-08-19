cf690cd7776223d1ae5f17ee8dea9bb499abd5c1
hemanth <hemanthtadikonda29@gmail.com>
2023-08-19 19:59:23 +0530

    vpc terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_vpc" "main" {
 }
 module "subnets" {
   for_each = var.subnets
-
   source = "./subnets"
   subnets = each.value
   vpc_id = aws_vpc.main.id
--- subnets/main.tf
+++ subnets/main.tf
@@ -1,10 +1,12 @@
 resource "aws_subnet" "main" {
+
   for_each          = var.subnets
+
   vpc_id            = var.vpc_id
   cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
 
   tags = {
-    Name = "each.key"
+    Name = each.key
   }
 }
