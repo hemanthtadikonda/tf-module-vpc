8148ed4f69e1355906cfc05a6fd42197ab76d87a
hemanth <hemanthtadikonda29@gmail.com>
2023-08-19 20:30:27 +0530

    vpc terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -18,12 +18,9 @@ resource "aws_route_table" "main" {
     Name = each.key
   }
 }
+resource "aws_route_table_association" "a" {
+  for_each = var.subnets
 
-
-output "subnets_id" {
-  value = aws_subnet.main
-}
-
-output "routetable_id" {
-  value = aws_route_table.main
+  subnet_id      = lookup(lookup(aws_subnet.main,each.key, null ), "id", null)
+  route_table_id = lookup(lookup(aws_route_table.main, each.key, null ), "id", null)
 }
