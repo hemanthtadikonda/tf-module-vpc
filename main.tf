5f547430085dcbfc13f5f55f10ffbb86bc6e75e3
hemanth <hemanthtadikonda29@gmail.com>
2023-08-21 20:09:25 +0530

    vpc terraform

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_nat_gateway" "ngw" {
 resource "aws_route" "ngw" {
   count                     = length(local.private_route_table_ids)
   route_table_id            = element(local.private_route_table_ids,count.index )
-  destination_cidr_block    = var.cidr
+  destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id            = element(aws_nat_gateway.ngw.*.id ,count.index )
 }
 
